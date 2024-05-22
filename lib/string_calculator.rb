dc59d5c8332638ace9e6386c965354ca20f5ca16
Banti Narvariya <bantinarvariya16@gmail.com>
2024-05-22 23:06:50 +0530

    Modified add to handles different delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,6 +6,11 @@ class StringCalculator
     return 0 if numbers.empty?
 
     delimiter = /,|\n/
+    if numbers.start_with?('//')
+      delimiter, numbers = numbers[2..].split("\n", 2)
+      delimiter = Regexp.escape(delimiter)
+    end
+
     nums = numbers.split(/#{delimiter}/).map(&:to_i)
     nums.sum
   end
